b5467b514b0c13ca90e6b4ab27b35f674e235d79
Aamir <djzaamir@gmail.com>
2021-10-15 08:46:56 +0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/ray_tracing_in_one_weekend_practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray_tracing_in_one_weekend_practice.dir/main.cpp.o"
+  "ray_tracing_in_one_weekend_practice"
+  "ray_tracing_in_one_weekend_practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray_tracing_in_one_weekend_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ray_tracing_in_one_weekend_practice.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ray_tracing_in_one_weekend_practice.
+# This may be replaced when dependencies are built.
