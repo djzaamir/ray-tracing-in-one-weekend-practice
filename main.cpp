ffd30bcde209f1281c596a05b692f5e8f87d1d00
Aamir <djzaamir@gmail.com>
2021-10-15 09:10:58 +0500

    Rendered a basic RGB image in PPM format, the standard output has to be redirected to an image file e.g image.ppm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    const int image_width  = 256;
+    const int image_height = 256;
+
+    //Writing the PPM image format header
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+
+   //Rendering a red background
+    for (int i = 0; i < image_height; ++i) {
+        for (int j = 0; j < image_width; ++j) {
+            if (i < (image_height /3))
+                std::cout << 255 << " " << 0 << " " << 0 << std::endl;
+            else if( i < ((image_height / 3) * 2))
+                std::cout << 0 << " " << 255 << " " << 0 << std::endl;
+            else
+                std::cout << 0 << " " << 0 << " " << 255 << std::endl;
+        }
+    }
+
     return 0;
 }
